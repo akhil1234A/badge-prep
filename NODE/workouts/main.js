6241748d68276f3662f1c1dd0d2aff2715c55606
Akhil Anwer M <akhilanwarm@gmail.com>
2025-01-09 23:03:08 +0530

    fs operations

--- NODE/workouts/main.js
+++ NODE/workouts/main.js
@@ -1,11 +1,28 @@
-const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req,res)=>{
-  res.statusCode = 200; 
-  res.setHeader('Content-Type','text/plain');
-  res.end('Hellodf');
+//Write, Read Sync
+
+fs.writeFileSync('sample.text','This is a sample file reading exercise');
+const data = fs.readFileSync('sample.text','utf-8');
+
+console.log(data);
+
+//Write, Read Async
+
+fs.writeFile('s','hi',(err)=>{
+  if (err) throw err;
+  console.log('success');
 })
 
-server.listen(3000,()=>{
-  console.log('server is running')
+fs.readFile('s','utf-8',(err,data)=>{
+  if(err) throw err; 
+  console.log(data);
+})
+
+//Current Date and Time
+
+const date = new Date().toString();
+
+fs.writeFile('s',date,(err)=>{
+  if(err) throw err;
 })
